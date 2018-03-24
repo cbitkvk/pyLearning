b03f53b5d58852e5022bfc5a93e08acccb8094f8
Vinay <vinaykumarcbit@yahoo.co.in>
2018-03-24 21:31:41 +0530

    integrated db

--- PycharmProjects/PyLearning/Stocks/Stock.py
+++ PycharmProjects/PyLearning/Stocks/Stock.py
@@ -1,27 +1,30 @@
 import urllib.request
+import urllib.request
 import traceback
 from PyLearning.Stocks.StockHtmlParser import StockHtmlParser
-
+import pymysql
+import logging
+from decimal import *
 
 def set_header_url():
     hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Referer': 'https://cssspritegenerator.com',
-        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-        'Accept-Encoding': 'none',
-        'Accept-Language': 'en-US,en;q=0.8',
-        'Connection': 'keep-alive'}
+           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+           'Referer': 'https://cssspritegenerator.com',
+           'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+           'Accept-Encoding': 'none','Accept-Language': 'en-US,en;q=0.8',
+           'Connection': 'keep-alive'}
     return hdr
 
 
 class Stock:
-    def __init__(self, stock_dict):
+    def __init__(self, stock_dict, logg):
+        self.mylogger = logg
+
         self.script_name = stock_dict['script_name']
         self.date_list = stock_dict['date_list'][0]
         self.exchange = stock_dict['exchange']
 
         # just for temporary to get rid of that warning.
-
         self.prev_close = None
         self.open_price = None
         self.high_price = None
@@ -33,7 +36,6 @@ class Stock:
         self.turnover = None
         self.no_of = None
         self.deliverable = None
-
 
     def get_price_variation(self):
         try:
@@ -61,7 +63,8 @@ class Stock:
 
         # url = """https://www.nseindia.com/live_market/dynaContent/live_watch/get_quote/getHistoricalData.jsp?symbol={}&series=EQ&fromDate=undefined&toDate=undefined&datePeriod=1day""".format(self.script_name)
         url = """https://www.nseindia.com/products/dynaContent/common/productsSymbolMapping.jsp?symbol={}&segmentLink=3&symbolCount=1&series=ALL&dateRange=day&fromDate=&toDate=&dataType=PRICEVOLUMEDELIVERABLE""".format(self.script_name)
-        print("URL:", url)
+        self.mylogger.info(msg="Started the download of {}".format(self.script_name))
+
         rqst = urllib.request.Request(url, headers=hdr)
         rsp = urllib.request.urlopen(rqst)
 
@@ -79,6 +82,7 @@ class Stock:
         if not "No Record Found" in data_html:
             prs.feed(data_html)
             self.set_class_variables(prs.stock_dict)
+            self.mylogger.info(msg="End of download of {}".format(self.script_name))
 
     def set_class_variables(self, stock_dict):
         self.prev_close = stock_dict['Prev Close']
@@ -91,8 +95,33 @@ class Stock:
         self.ttl_trd = stock_dict['Total Traded  Quantity']
         self.turnover = stock_dict['Turnover ']
         self.no_of = stock_dict['No. of  Trades']
-        self.deliverable = stock_dict['Deliverable Qty']
-        #self.dly_qt_to = stock_dict['% Dly Qt to Traded Qty']
+        self.deliverable = stock_dict['Deliverable Qty'] if stock_dict['Deliverable Qty'] != "-" else None
+        # self.dly_qt_to = stock_dict['% Dly Qt to Traded Qty']
 
+    def get_connection_details(self):
+        return pymysql.connect(host='localhost', user='root', password='vinay', db='stocks',
+                               charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
 
+    def write_to_db(self):
+        connection = self.get_connection_details()
+        sql = "insert into stocks.stock_names values(%s, %s, %s)"
+        print("wrinting to db")
+        r = connection.cursor()
+        r.execute(sql, args=(1, self.script_name, self.exchange))
+        sql2 = "insert into stocks.script_detailed_info values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        # r.execute(sql2, args=(self.script_name, Decimal(self.prev_close.replace(",","")), Decimal(self.open_price.replace(",","")), Decimal(self.high_price.replace(",","")), Decimal(self.low_price.replace(",","")), Decimal(self.last_price.replace(",","")), Decimal(self.close_price.replace(",","")), Decimal(self.vwap.replace(",","")), Decimal(self.ttl_trd.replace(",","")), Decimal(self.turnover.replace(",","")), Decimal(self.no_of.replace(",","")), Decimal(self.deliverable.replace(",",""))))
+        r.execute(sql2, args=(self.script_name, self.convert_decimal(self.prev_close),
+                              self.convert_decimal(self.open_price),
+                              self.convert_decimal(self.high_price),
+                              self.convert_decimal(self.low_price),
+                              self.convert_decimal(self.last_price),
+                              self.convert_decimal(self.close_price),
+                              self.convert_decimal(self.vwap),
+                              self.convert_decimal(self.ttl_trd),
+                              self.convert_decimal(self.turnover),
+                              self.convert_decimal(self.no_of),
+                              self.convert_decimal(self.deliverable)))
+        connection.commit()
 
+    def convert_decimal(self, val):
+        return None if (val == None or val == "-") else val.replace(",", "")
--- PycharmProjects/PyLearning/secondDir/buildStocksData.py
+++ PycharmProjects/PyLearning/secondDir/buildStocksData.py
@@ -1,6 +1,8 @@
 from PyLearning.Stocks.Stock import *
 import logging
 import ast
+import pickle
+import pymysql
 
 def set_header_url():
     hdr = {
@@ -13,51 +15,74 @@ def set_header_url():
     return hdr
 
 
-symbol = "INFY"
-start_date = "01-03-2018"
-end_date = "20-03-2018"
-
-url = """https://www.nseindia.com/products/dynaContent/common/productsSymbolMapping.jsp?symbol={}&
-segmentLink=3&symbolCount=1&series=ALL&dateRange=+&fromDate={}&toDate={}&
-dataType=PRICEVOLUMEDELIVERABLE""".format(symbol, start_date, end_date)
+# url = """https://www.nseindia.com/products/dynaContent/common/productsSymbolMapping.jsp?symbol={}&
+# segmentLink=3&symbolCount=1&series=ALL&dateRange=+&fromDate={}&toDate={}&
+# dataType=PRICEVOLUMEDELIVERABLE""".format(symbol, start_date, end_date)
 
 
 def set_logger(logger_object, module_name):
     logger_object.setLevel(logging.INFO)
+    formatter = logging.Formatter("[ %(threadName)s %(asctime)s %(levelname)s ] %(msg)s")
 
     log_file = logging.FileHandler(filename="D://pythonLogging//" + module_name + ".log", mode="w")
     log_file.setLevel(logging.INFO)
+    log_file.setFormatter(formatter)
 
     stdout = logging.StreamHandler()
     stdout.setLevel(logging.INFO)
+    stdout.setFormatter(formatter)
 
     err_file = logging.FileHandler(filename="D://pythonLogging//" + module_name + ".err", mode="w")
     err_file.setLevel(logging.ERROR)
+    err_file.setFormatter(formatter)
 
     logger_object.addHandler(log_file)
     logger_object.addHandler(err_file)
     logger_object.addHandler(stdout)
+    return logger_object
+
+
+def tables_cleanup():
+    con = pymysql.connect(host='localhost', user='root', password='vinay', db='stocks',
+                               charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+    p = con.cursor()
+    sql = "delete from stocks.script_detailed_info"
+    sql2 = "delete from stocks.stock_names"
+    p.execute(sql)
+    p.execute(sql2)
+    con.commit()
+
 
 
 def main(args):
     process_logger = logging.getLogger(__name__)
     mod_name = __file__.__str__().split("/")[-1].replace(".py", "")
     set_logger(process_logger, mod_name)
+    process_logger.info(msg="Started db cleanup")
+    tables_cleanup()
+    process_logger.info(msg="ended db cleanup")
     # p = Stock({"script_name": "INFY", "date_list": ["212"] , "exchange": "NSE"}).download_stock_price()
     # print(p)
 
     # Read the file containing list of stocks on NSE.
     list_of_stocks_str = open("C:\\Users\\Dell\\Desktop\\stock_list_nse.txt").read()
     list_of_stocks = ast.literal_eval(list_of_stocks_str)
+    date_list = ["2018-03-23"]
+    exchange = "NSE"
     stocks_dict = {}
+    id = 1
     for stk in list_of_stocks:
-        p = Stock({"script_name": stk, "date_list": ["212"], "exchange": "NSE"})
+        p = Stock({"script_name": stk, "date_list": date_list, "exchange": exchange}, process_logger)
         p.download_stock_price()
+        p.write_to_db()
         # print(p)
         stocks_dict[stk] = p
 
     for stk, dct in stocks_dict.items():
         print(stk, "->", dct)
+
+    pickle.dump(list_of_stocks, file=open("D://marketdata//todayJson.pkl", 'wb'))
+
 
 if __name__ == "__main__":
     main("")
